86c008d2e2a359fe84d3f149e8267b32c9da51c5
sophia-h-le <61862324+sophia-h-le@users.noreply.github.com>
2021-03-02 16:06:44 -0800

    Add files via upload

--- moveAlgorithm.js
+++ moveAlgorithm.js
@@ -1,7 +1,16 @@
 //to decide where to make a move at each turn
-//iterate through all available cells
-    //calculate the Score of each cell
-    //make a move at the cell with highest score
+    //iterate through all available cells
+        //calculate the Score of each cell for mover and opponent
+        //compare the highest Score for mover and opponnent
+            //if mover >= opponent, move to increase mover Score
+            //else move to hinder opponent Score
 
-//how to calculate the Score of a cell
-//from that cell, move in each direction
+        //multiple cells: calculate sum of mover and opponent Score of those cells
+        //pick the highest Score
+
+        //multiple cells: calculate distance to the center
+
+        //multiple cells: random
+//make the move
+
+
