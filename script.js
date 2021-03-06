1c30b56b50a87f0a130099c3266ca6ec3fe496b9
sophia-h-le <61862324+sophia-h-le@users.noreply.github.com>
2021-03-05 16:03:00 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -78,20 +78,9 @@ var canvas = document.getElementById("myCanvas");
 var padding = 0;
 var cellSide = 40;
 var radius = 10;
+var winningRequirement = 5;
 var userChoseX = false;
 
-// canvas.addEventListener(
-//     'click', function(evt) {
-//         var cursorPos = getCursorPosition(canvas, evt);
-//         console.log(cursorPos);
-//         var centerPos = getCenterPosition(cursorPos, cellSide);
-//         console.log(centerPos);
-//         makeUserMove(userChoseX, canvas, centerPos, radius);
-//     }, false
-// );
-
-
-//game logic
 const Winner = {"cross": 1, "naught": 2, "tie": 3, "unidentified": 4};
 Object.freeze(Winner);
 
@@ -153,7 +142,7 @@ function Game(boardWidth, boardHeight) {
             }
 
             var totalCount = count1 + 1 + count2;
-            if (totalCount >= 5) {
+            if (totalCount >= winningRequirement) {
                 this.winner = mover;
             }
         }
@@ -171,8 +160,8 @@ function Game(boardWidth, boardHeight) {
 function drawGame(canvas, game) {
     drawGrid(canvas, padding, cellSide);
     var currentBoard = game.getBoard();
-    for (var i = 0; i < game.width; i++) {
-        for (var j = 0; j < game.height; j++) {
+    for (var i = 0; i < game.height; i++) {
+        for (var j = 0; j < game.width; j++) {
             var cellPos = {
                 x: i,
                 y: j
@@ -186,7 +175,20 @@ function drawGame(canvas, game) {
     }
 }
 
-//main
+function getWinnerAnnouncement(game, Winner, document) {
+    var announcement;
+    var winner = game.getWinner();
+    if (winner == Winner.cross) {
+        announcement = 'Winner is CROSS';
+    } else if (winner == Winner.naught) {
+        announcement = 'Winner is NAUGHT';
+    } else if (winner == Winner.tie) {
+        announcement = "It's a TIE";
+    }
+    document.getElementById('winner-announcement').innerHTML = announcement;
+    document.getElementById('winner-announcement').style.visibility = 'visible';
+}
+
 var game = new Game(15, 15);
 drawGame(canvas, game);
 canvas.addEventListener(
@@ -197,6 +199,9 @@ canvas.addEventListener(
         game.move(game.currentMover, cellPos.x, cellPos.y);
         // game.checkWinner(mover, cellPos.x, cellPos.y);
         // makeUserMove(userChoseX, canvas, centerPos, radius);
+        machineMove = getAutoMove(game);
+        console.log(machineMove);
+        game.move(game.currentMover, machineMove[0], machineMove[1]);
         clearCanvas(canvas);
         drawGame(canvas, game);
         
@@ -204,6 +209,7 @@ canvas.addEventListener(
         if (winner != Winner.unidentified) {
             // remove event listener function `main`
             canvas.removeEventListener('click', main);
+            getWinnerAnnouncement(game, Winner, document);
         }
     }, false
 );
@@ -215,4 +221,188 @@ document.getElementById("newGame").addEventListener(
         drawGame(canvas, game);
     }, false
 ); 
+
+//AIMOve
+function calculateCellScore(game, x, y, mover) {
+    var board = game.getBoard();
+    const directions = [[-1, 0], [-1, -1], [0, -1], [-1, 1]];
+    var cellScore = 0;
+    
+    for (var i = 0; i < directions.length; i++) {
+        var currentX = x + directions[i][0];
+        var currentY = y + directions[i][1];
+        var score1 = 0;
+
+        while (currentX >= 0 && currentX < game.height && currentY >= 0 && currentY < game.width) {
+            if (board[currentX][currentY] == mover) {
+                score1++;
+                currentX += directions[i][0];
+                currentY += directions[i][1];
+            } else {
+                break;
+            }
+        }
+
+        currentX = x - directions[i][0];
+        currentY = y - directions[i][1];
+        var score2 = 0;
+        while (currentX >= 0 && currentX < game.height && currentY >= 0 && currentY < game.width) {
+            if (board[currentX][currentY] == mover) {
+                score2++;
+                currentX -= directions[i][0];
+                currentY -= directions[i][1];
+            } else {
+                break;
+            }
+        }
+        var totalScore = score1 + score2;
+        if (totalScore > cellScore) {
+            cellScore = totalScore;
+        }
+    }
+    if (board[x][y]) {
+        cellScore = -1;
+    }
+
+    return cellScore;
+}
+
+//function checked
+function playerPotentialCellScores(game, player) {
+    playerCellScores = [];
+    for (var i = 0; i < game.height; i++) {
+        playerCellScores[i] = [];
+    }
+    for (var i = 0; i < game.height; i++) {
+        for (var j = 0; j < game.width; j++) {
+            var cellScore = calculateCellScore(game, i, j, player);
+            playerCellScores[i][j] = cellScore;
+        }
+    }
+    return playerCellScores;
+}
+
+//function checked
+function getMaxPotentialScores(playerCellScores) {
+    var maxScoreCells = [];
+    var maxCellScore = 0;
+    for (var i = 0; i < playerCellScores.length; i++) {
+        for (var j = 0; j < playerCellScores[0].length; j++) {
+            var cellScore = playerCellScores[i][j];
+            var cell = [i, j];
+            if (cellScore > maxCellScore) {
+                maxCellScore = cellScore;
+                maxScoreCells.length = 0;
+                maxScoreCells.push(cell);
+            } else if (cellScore == maxCellScore) {
+                maxScoreCells.push(cell);
+            }
+        }
+    }
+    return {
+        score: maxCellScore,
+        cells: maxScoreCells
+    };
+}
+
+//function checked
+function getCellTotalScores(game, selfCellScores, oppCellScores) {
+    var cellTotalScores = [];
+    for (var i = 0; i < game.height; i++) {
+        var rowTotalScores = [];
+        for (var j = 0; j < game.width; j++) {
+            var selfScore = selfCellScores[i][j];
+            var oppScore = oppCellScores[i][j];
+            var cellTS = selfScore + oppScore;
+            rowTotalScores.push(cellTS);
+        }
+        cellTotalScores.push(rowTotalScores);
+    }
+    return cellTotalScores;
+}
+
+//function checked
+function getMaxTotalScoreCells(nextMoveCells, cellTotalScores) {
+    var maxTotalScore = 0;
+    var maxTSCells = [];
+    for (var id in nextMoveCells) {
+        var cell = nextMoveCells[id];
+        var x = cell[0];
+        var y = cell[1];
+        var cellTS = cellTotalScores[x][y];
+
+        if (cellTS > maxTotalScore) {
+            maxTotalScore = cellTS;
+            maxTSCells.length = 0;
+            maxTSCells.push(cell);
+        } else if (cellTS == maxTotalScore) {
+            maxTSCells.push(cell);
+        }
+    }
+    return maxTSCells;
+}
+
+function calculateDistance(x1, y1, x2, y2) {
+    var xDistance = Math.abs(x1 - x2)
+    var yDistance = Math.abs(y1 - y2);
+    var distance = Math.max(xDistance, yDistance);
+    return distance;
+}
+
+function getNearCenterCells(game, nextMoveCells) {
+    var centerX = Math.floor(game.height / 2);
+    var centerY = Math.floor(game.width / 2);
+    var nearCenterCells = [];
+    var minDistance = Math.max(centerX, centerY);
+    for (var id in nextMoveCells) {
+        var cell = nextMoveCells[id];
+        var x = cell[0];
+        var y = cell[1];
+        var distance = calculateDistance(x, y, centerX, centerY);
+        if (distance < minDistance) {
+            nearCenterCells.length = 0;
+            minDistance = distance;
+            nearCenterCells.push(cell);
+        } else if (distance == minDistance) {
+            nearCenterCells.push(cell);
+        }
+    }
+    return nearCenterCells;
+}
+
+function getAutoMove(game) {
+    var nextMoveCells = [];
+
+    var self = game.getCurrentMover();
+    var selfCellScores = playerPotentialCellScores(game, self);
+    var selfMPT = getMaxPotentialScores(selfCellScores);
+    var selfScore = selfMPT.score;
+    var selfCells = selfMPT.cells;
+
+    var opponent = (game.getCurrentMover() % 2) + 1;
+    var oppCellScores = playerPotentialCellScores(game, opponent);
+    var oppMPT = getMaxPotentialScores(oppCellScores);
+    var oppScore = oppMPT.score;
+    var oppCells = oppMPT.cells;
+
+    // first elimination
+    (selfScore >= oppScore) ? nextMoveCells = selfCells : nextMoveCells = oppCells;
+
+
+    var cellTotalScores = getCellTotalScores(game, selfCellScores, oppCellScores);
+    // second elimination
+    var maxTSCells = getMaxTotalScoreCells(nextMoveCells, cellTotalScores);
+    nextMoveCells = maxTSCells;
+
+    
+    //third elimination
+    var nearCenterCells = getNearCenterCells(game, nextMoveCells);
+    nextMoveCells = nearCenterCells;
+
+    //last elimination: randomization
+    // var random = items[Math.floor(Math.random()*items.length)]
+    var chosenMove = nextMoveCells[Math.floor(Math.random() * nextMoveCells.length)];
+    return chosenMove;
+}
+
 
